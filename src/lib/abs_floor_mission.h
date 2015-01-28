83fa40cfac9c22eac28627b5ae65e2ecc624e66d
K-McK <odyssey4444@sbcglobal.net>
2015-01-27 18:17:01 -0600

    1/27/15

--- src/lib/abs_floor_mission.h
+++ src/lib/abs_floor_mission.h
@@ -68,52 +68,52 @@ void abs_floor_mission()
 			abs_second_objective(CENTER_GOAL);
 
 			break;
-		case 2:
-			abs_drive(BACKWARD, E_ANGLE, 60, 60, true, GYRO, DONT_SLOW_DOWN);
-			abs_turn(COUNTERCLOCKWISE, POINT, TURN, 31, 40);
+		//case 2:
+		//	abs_drive(BACKWARD, E_ANGLE, 60, 60, true, GYRO, DONT_SLOW_DOWN);
+		//	abs_turn(COUNTERCLOCKWISE, POINT, TURN, 31, 40);
 
-			abs_drive(BACKWARD, E_ANGLE, 130, 60, true, GYRO, DONT_SLOW_DOWN);
+		//	abs_drive(BACKWARD, E_ANGLE, 130, 60, true, GYRO, DONT_SLOW_DOWN);
 
-			g_auto_lift_done = false;
-			StartTask(abs_auto_center_pipe_score);
-			wait1Msec(200);
-			abs_turn(CLOCKWISE, POINT, TURN, 74, 40);
+		//	g_auto_lift_done = false;
+		//	StartTask(abs_auto_center_pipe_score);
+		//	wait1Msec(200);
+		//	abs_turn(CLOCKWISE, POINT, TURN, 74, 40);
 
-			abs_drive(BACKWARD, E_ANGLE, 44, 40, true, GYRO, DONT_SLOW_DOWN);
+		//	abs_drive(BACKWARD, E_ANGLE, 44, 40, true, GYRO, DONT_SLOW_DOWN);
 
-			while(!g_auto_lift_done){}
-			StopTask(abs_auto_center_pipe_score);
+		//	while(!g_auto_lift_done){}
+		//	StopTask(abs_auto_center_pipe_score);
 
-			wait1Msec(400);
+		//	wait1Msec(400);
 
-			servo[shutter] = g_shutter_closed;
+		//	servo[shutter] = g_shutter_closed;
 
-			abs_second_objective(CENTER_GOAL);
-			break;
-		case 3:
-			abs_drive(BACKWARD, E_ANGLE, 60, 60, true, GYRO, DONT_SLOW_DOWN);
-			abs_turn(CLOCKWISE, POINT, TURN, 31, 40);
+		//	abs_second_objective(CENTER_GOAL);
+		//	break;
+		//case 3:
+		//	abs_drive(BACKWARD, E_ANGLE, 60, 60, true, GYRO, DONT_SLOW_DOWN);
+		//	abs_turn(CLOCKWISE, POINT, TURN, 31, 40);
 
-			g_auto_lift_done = false;
-			StartTask(abs_auto_center_pipe_score);
-			abs_drive(BACKWARD, E_ANGLE, 78, 60, true, GYRO, DONT_SLOW_DOWN);
+		//	g_auto_lift_done = false;
+		//	StartTask(abs_auto_center_pipe_score);
+		//	abs_drive(BACKWARD, E_ANGLE, 78, 60, true, GYRO, DONT_SLOW_DOWN);
 
-			wait1Msec(200);
-			abs_turn(COUNTERCLOCKWISE, POINT, TURN, 33, 40);
+		//	wait1Msec(200);
+		//	abs_turn(COUNTERCLOCKWISE, POINT, TURN, 33, 40);
 
-			abs_drive(BACKWARD, E_ANGLE, 16, 60, true, GYRO, DONT_SLOW_DOWN);
+		//	abs_drive(BACKWARD, E_ANGLE, 16, 60, true, GYRO, DONT_SLOW_DOWN);
 
-			servo[shutter] = 155;
+		//	servo[shutter] = 155;
 
-			while(!g_auto_lift_done){}
-			StopTask(abs_auto_center_pipe_score);
+		//	while(!g_auto_lift_done){}
+		//	StopTask(abs_auto_center_pipe_score);
 
-			wait1Msec(200);
+		//	wait1Msec(200);
 
-			servo[shutter] = g_shutter_closed;
+		//	servo[shutter] = g_shutter_closed;
 
-			abs_second_objective(CENTER_GOAL);
-			break;
+		//	abs_second_objective(CENTER_GOAL);
+		//	break;
 		default:
 			while(true)
 			{
